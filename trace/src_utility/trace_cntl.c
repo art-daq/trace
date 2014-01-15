62de216d2c0e8c63a213d92249ccf93e53b1bf6e
ron <>
2014-01-15 21:12:31 +0000

    massage to get rid of unused-function warning

--- trace/src_utility/trace_cntl.c
+++ trace/src_utility/trace_cntl.c
@@ -3,7 +3,7 @@
     or COPYING file. If you do not have such a file, one can be obtained by
     contacting Ron or Fermi Lab in Batavia IL, 60510, phone: 630-840-3000.
     $RCSfile: trace_cntl.c,v $
-    rev="$Revision: 1.2 $$Date: 2014/01/15 20:04:16 $";
+    rev="$Revision: 1.3 $$Date: 2014/01/15 21:12:31 $";
     */
 /*
 gxx_standards.sh Trace_test.c
@@ -26,7 +26,9 @@ main(  int	argc
     TRACE( 2, "hello %d %d", 1, 2 );
     TRACE( 3, "hello %d %d %d", 1,2,3 );
 
+#  ifndef TEST_UNUSED_FUNCTION
     TRACE_CNTL( "trig", -1, 5 );
+#  endif
     for (ii=0; ii<20; ++ii)
 	TRACE( 0, "ii=%u", ii );
     return (0);

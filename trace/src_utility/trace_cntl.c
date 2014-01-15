97967d9049cab94d21e8bbc997cf5f727333c646
ron <>
2014-01-15 20:04:16 +0000

    add traceCntl

--- trace/src_utility/trace_cntl.c
+++ trace/src_utility/trace_cntl.c
@@ -3,7 +3,7 @@
     or COPYING file. If you do not have such a file, one can be obtained by
     contacting Ron or Fermi Lab in Batavia IL, 60510, phone: 630-840-3000.
     $RCSfile: trace_cntl.c,v $
-    rev="$Revision: 1.1 $$Date: 2014/01/15 15:25:03 $";
+    rev="$Revision: 1.2 $$Date: 2014/01/15 20:04:16 $";
     */
 /*
 gxx_standards.sh Trace_test.c
@@ -19,9 +19,15 @@ int
 main(  int	argc
      , char	*argv[] )
 {
+    unsigned ii;
+
     TRACE( 0, "hello" );
     TRACE( 1, "hello %d", 1 );
     TRACE( 2, "hello %d %d", 1, 2 );
     TRACE( 3, "hello %d %d %d", 1,2,3 );
+
+    TRACE_CNTL( "trig", -1, 5 );
+    for (ii=0; ii<20; ++ii)
+	TRACE( 0, "ii=%u", ii );
     return (0);
 }   /* main */

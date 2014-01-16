adcf96c3c3e54b84a395988ebb3454f40b39d984
ron <ron@f0fafde3-ccc7-4eb4-bb98-6daa41875d3e>
2014-01-16 06:36:08 +0000

    start of mmap file trace
    git-svn-id: svn+ssh://cdcvs.fnal.gov/cvs/projects/trace-svn/trunk@56 f0fafde3-ccc7-4eb4-bb98-6daa41875d3e

--- trace/src_utility/trace_cntl.c
+++ trace/src_utility/trace_cntl.c
@@ -3,7 +3,7 @@
     or COPYING file. If you do not have such a file, one can be obtained by
     contacting Ron or Fermi Lab in Batavia IL, 60510, phone: 630-840-3000.
     $RCSfile: trace_cntl.c,v $
-    rev="$Revision: 1.3 $$Date: 2014-01-15 21:12:31 $";
+    rev="$Revision: 1.4 $$Date: 2014-01-16 06:36:08 $";
     */
 /*
 gxx_standards.sh Trace_test.c
@@ -13,7 +13,12 @@ done
    
 */
 #include <stdio.h>		/* printf */
-#include "Trace.h"
+#include <libgen.h>		/* basename */
+#include "Trace_mmap.h"
+
+#define USAGE "\
+%s <cmd> [command opt/args]\n\
+", basename(argv[0])
 
 int
 main(  int	argc
@@ -21,15 +26,56 @@ main(  int	argc
 {
     unsigned ii;
 
-    TRACE( 0, "hello" );
-    TRACE( 1, "hello %d", 1 );
-    TRACE( 2, "hello %d %d", 1, 2 );
-    TRACE( 3, "hello %d %d %d", 1,2,3 );
-
-#  ifndef TEST_UNUSED_FUNCTION
-    TRACE_CNTL( "trig", -1, 5 );
+#  if 0
+    int         opt;
+    while (1)
+    {   int option_index = 0;
+        static struct option long_options[] =
+        {   /* name,   has_arg, int* flag, val_for_flag */
+            {0,            0,      0,          0}
+        };
+        opt = getopt_long (argc, argv, "?hn:s:p",
+                        long_options, &option_index);
+        if (opt == -1) break;
+        switch (opt)
+        {
+        case '?': case 'h':  printf( USAGE );  exit( 0 ); break;
+        default:  printf ("?? getopt returned character code 0%o ??\n", opt);
+        }
+    }
+    if (argc - optind != 1)
+    {   printf( "Need cmd\n" );
+        printf( USAGE ); exit( 0 );
+    }
 #  endif
-    for (ii=0; ii<20; ++ii)
-	TRACE( 0, "ii=%u", ii );
+
+    printf( "(unsigned long)-1=0x%lx\n", (unsigned long)-1 );
+
+    if (argc <= 1) { printf(USAGE); exit(0); }
+
+    if      (strncmp(argv[1],"test",4) == 0)
+    {
+	TRACE( 0, "hello" );
+	TRACE( 1, "hello %d", 1 );
+	TRACE( 2, "hello %d %d", 1, 2 );
+	TRACE( 3, "hello %d %d %d", 1,2,3 );
+
+#      ifndef TEST_UNUSED_FUNCTION
+	TRACE_CNTL( "trig", -1, 5 );
+#      endif
+	for (ii=0; ii<20; ++ii)
+	    TRACE( 0, "ii=%u", ii );
+    }
+    else if (strncmp(argv[1],"info",4) == 0) 
+    {
+	TRACE_CNTL( argv[1] );
+    }
+    else if (strcmp(argv[1],"reset") == 0) 
+    {
+	TRACE_CNTL( argv[1] );
+    }
+    else
+    {   printf("invalid command\n" USAGE );
+    }
     return (0);
 }   /* main */

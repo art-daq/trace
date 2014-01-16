98a169d33a9445326da2b1b032a3f328d2a5df23
ron <>
2014-01-16 18:41:12 +0000

    snapshot

--- trace/src_utility/trace_cntl.c
+++ trace/src_utility/trace_cntl.c
@@ -3,7 +3,7 @@
     or COPYING file. If you do not have such a file, one can be obtained by
     contacting Ron or Fermi Lab in Batavia IL, 60510, phone: 630-840-3000.
     $RCSfile: trace_cntl.c,v $
-    rev="$Revision: 1.4 $$Date: 2014/01/16 06:36:08 $";
+    rev="$Revision: 1.5 $$Date: 2014/01/16 18:41:12 $";
     */
 /*
 gxx_standards.sh Trace_test.c
@@ -49,11 +49,23 @@ main(  int	argc
     }
 #  endif
 
-    printf( "(unsigned long)-1=0x%lx\n", (unsigned long)-1 );
 
     if (argc <= 1) { printf(USAGE); exit(0); }
 
-    if      (strncmp(argv[1],"test",4) == 0)
+
+    if      (strncmp(argv[1],"info",4) == 0) 
+    {
+	TRACE_CNTL( argv[1] );
+    }
+    else if (strcmp(argv[1],"reset") == 0) 
+    {
+	TRACE_CNTL( argv[1] );
+    }
+    else if (strcmp(argv[1],"show") == 0) 
+    {
+	TRACE_CNTL( argv[1] );
+    }
+    else if (strncmp(argv[1],"test",4) == 0)
     {
 	TRACE( 0, "hello" );
 	TRACE( 1, "hello %d", 1 );
@@ -61,18 +73,10 @@ main(  int	argc
 	TRACE( 3, "hello %d %d %d", 1,2,3 );
 
 #      ifndef TEST_UNUSED_FUNCTION
-	TRACE_CNTL( "trig", -1, 5 );
+	TRACE_CNTL( "trig", 3, -1, 5 );
 #      endif
 	for (ii=0; ii<20; ++ii)
 	    TRACE( 0, "ii=%u", ii );
-    }
-    else if (strncmp(argv[1],"info",4) == 0) 
-    {
-	TRACE_CNTL( argv[1] );
-    }
-    else if (strcmp(argv[1],"reset") == 0) 
-    {
-	TRACE_CNTL( argv[1] );
     }
     else
     {   printf("invalid command\n" USAGE );

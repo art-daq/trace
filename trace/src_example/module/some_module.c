fe1c572562a1fadddb2bf2b4cc83c20ff5e045ef
ron <ron@f0fafde3-ccc7-4eb4-bb98-6daa41875d3e>
2014-02-04 00:39:29 +0000

    add work queue
    git-svn-id: svn+ssh://cdcvs.fnal.gov/cvs/projects/trace-svn/trunk@118 f0fafde3-ccc7-4eb4-bb98-6daa41875d3e

--- trace/src_example/module/some_module.c
+++ trace/src_example/module/some_module.c
@@ -3,7 +3,7 @@
     or COPYING file. If you do not have such a file, one can be obtained by
     contacting Ron or Fermi Lab in Batavia IL, 60510, phone: 630-840-3000.
     $RCSfile: some_module.c,v $
-    rev="$Revision: 1.1 $$Date: 2014-01-31 04:24:43 $";
+    rev="$Revision: 1.2 $$Date: 2014-02-04 00:39:29 $";
     */
 
 // NOTE: this is trace_.c and not trace.c because nfs server has case
@@ -12,10 +12,24 @@
 #include <linux/module.h>	// module_param, THIS_MODULE
 #include <linux/init.h>		// module_init,_exit
 #include <linux/kernel.h>	// KERN_INFO, printk
+#include <linux/jiffies.h>
+#include <linux/delay.h>	/* msleep */
 
 
 #include "../module3/Trace_mmap4.h"
 
+static struct workqueue_struct *wq=0; 
+static int                      stop_requested=0;
+
+static void my_work_function( struct work_struct *w );
+static DECLARE_WORK(my_work, my_work_function);
+static void my_work_function( struct work_struct *w )
+{
+    TRACE( 0,"hello from my_work_function" );
+    msleep( 1500 );
+    if (!stop_requested)
+	queue_work( wq, &my_work );    
+}
 
  
 static int __init init_some_module(void)
@@ -26,14 +40,24 @@ static int __init init_some_module(void)
 
     TRACE( 2, "init_some_module trace" );
 
+    wq = create_singlethread_workqueue("mywork");
+    if (wq)
+	queue_work( wq, &my_work );
+
     return (ret);
 }   // init_some_module
 
 static void __exit exit_some_module(void)
 {
+    printk(KERN_INFO "exit_some_module() called\n");
 
-        printk(KERN_INFO "exit_some_module() called\n");
-
+    if (wq)
+    {   printk("requesting stop\n");
+	stop_requested = 1;
+	flush_workqueue(wq);
+	printk("workqueue stopped/flushed\n");
+	destroy_workqueue(wq);
+    }
 }   // exit_some_module
 
 module_init(init_some_module);

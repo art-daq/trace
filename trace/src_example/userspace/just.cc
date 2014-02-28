9aa3e37243b34d07282eee9656d7e34223a00d83
ron <>
2014-02-28 03:06:48 +0000

    allow just.cc to be compiled -std=c89

--- trace/src_example/userspace/just.cc
+++ trace/src_example/userspace/just.cc
@@ -1,11 +1,12 @@
- // This file (just.cc) was created by Ron Rechenmacher <ron@fnal.gov> on
- // Feb 19, 2014. "TERMS AND CONDITIONS" governing this file are in the README
- // or COPYING file. If you do not have such a file, one can be obtained by
- // contacting Ron or Fermi Lab in Batavia IL, 60510, phone: 630-840-3000.
- // $RCSfile: just.cc,v $
- // rev="$Revision: 1.1 $$Date: 2014/02/19 18:55:47 $";
+/* This file (just.cc) was created by Ron Rechenmacher <ron@fnal.gov> on
+// Feb 19, 2014. "TERMS AND CONDITIONS" governing this file are in the README
+// or COPYING file. If you do not have such a file, one can be obtained by
+// contacting Ron or Fermi Lab in Batavia IL, 60510, phone: 630-840-3000.
+// $RCSfile: just.cc,v $
+// rev="$Revision: 1.2 $$Date: 2014/02/28 03:06:48 $";
+*/
 
-#include "trace.h"		// TRACE
+#include "trace.h"		/* TRACE */
 
 int
 main(  int	argc
@@ -13,4 +14,4 @@ main(  int	argc
 {
     TRACE( 1, "hello - hopefully no warnings" );
     return (0);
-}   // main
+}   /* main */

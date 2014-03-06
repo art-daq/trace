fc941bce14a4eae14932a6aacef7bb6dceeb4cc5
ron <>
2014-03-06 18:45:18 +0000

    indicate - hopefully no _compile_ warnings

--- trace/src_example/userspace/just_user.cc
+++ trace/src_example/userspace/just_user.cc
@@ -3,7 +3,7 @@
 // or COPYING file. If you do not have such a file, one can be obtained by
 // contacting Ron or Fermi Lab in Batavia IL, 60510, phone: 630-840-3000.
 // $RCSfile: just_user.cc,v $
-// rev="$Revision: 1.1 $$Date: 2014/03/06 18:33:48 $";
+// rev="$Revision: 1.2 $$Date: 2014/03/06 18:45:18 $";
 */
 
 #define TRACE_LOG_FUNCTION(tvp,tid,lvl,msg,ap) vprintf( msg, ap );printf("\n")
@@ -13,6 +13,6 @@ int
 main(  int	argc
      , char	*argv[] )
 {
-    TRACE( 0, "hello - hopefully no warnings %d %.1f %d",1,1.5,2 );
+    TRACE( 0, "hello - hopefully no compile warnings %d %.1f %d",1,1.5,2 );
     return (0);
 }   /* main */

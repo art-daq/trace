ac444c2c5f284814745b9b8413452ee2faccd6fd
ron <ron@f0fafde3-ccc7-4eb4-bb98-6daa41875d3e>
2014-03-12 22:49:10 +0000

    tracelib.h, trace_no_printk mod param, TRACE_LVLS
    git-svn-id: svn+ssh://cdcvs.fnal.gov/cvs/projects/trace-svn/trunk@221 f0fafde3-ccc7-4eb4-bb98-6daa41875d3e

---
+++ trace/src_example/userspace/example_sub2.cc
@@ -0,0 +1,12 @@
+ // This file (example_obj.cc) was created by Ron Rechenmacher <ron@fnal.gov> on
+ // Mar 12, 2014. "TERMS AND CONDITIONS" governing this file are in the README
+ // or COPYING file. If you do not have such a file, one can be obtained by
+ // contacting Ron or Fermi Lab in Batavia IL, 60510, phone: 630-840-3000.
+ // $RCSfile: example_sub2.cc,v $
+ // rev="$Revision: 1.1 $$Date: 2014-03-12 22:49:10 $";
+
+#include "tracelib.h"
+
+void example_sub2( void )
+{   TRACE( 0, "hello from example_sub2()" );
+}

ed59e207e86de23ae1db358c6b1b15177ef73458
ron <ron@f0fafde3-ccc7-4eb4-bb98-6daa41875d3e>
2014-04-28 16:31:10 +0000

    add to example
    git-svn-id: svn+ssh://cdcvs.fnal.gov/cvs/projects/trace-svn/trunk@309 f0fafde3-ccc7-4eb4-bb98-6daa41875d3e

--- trace/src_example/userspace/just.cc
+++ trace/src_example/userspace/just.cc
@@ -3,9 +3,10 @@
 // or COPYING file. If you do not have such a file, one can be obtained by
 // contacting Ron or Fermi Lab in Batavia IL, 60510, phone: 630-840-3000.
 // $RCSfile: just.cc,v $
-// rev="$Revision: 1.4 $$Date: 2014-03-06 18:45:18 $";
+// rev="$Revision: 1.5 $$Date: 2014-04-28 16:31:10 $";
 */
 
+#include <string>
 #include "trace.h"		/* TRACE */
 
 int
@@ -13,5 +14,6 @@ main(  int	argc
      , char	*argv[] )
 {
     TRACE( 0, "hello - hopefully no compile warnings %d %.1f %d",1,1.5,2 );
+    TRACE( 0, (std::string("hi %d")+" there").c_str(), 2 );
     return (0);
 }   /* main */

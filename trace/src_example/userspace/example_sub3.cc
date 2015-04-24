9dd9f801bd20d7e647e7a02ce2c5e430c844a10b
ron <>
2015-04-24 17:05:47 +0000

    added more modules/sub-routines in userspace example

---
+++ trace/src_example/userspace/example_sub3.cc
@@ -0,0 +1,17 @@
+ // This file (example_obj.cc) was created by Ron Rechenmacher <ron@fnal.gov> on
+ // Mar 12, 2014. "TERMS AND CONDITIONS" governing this file are in the README
+ // or COPYING file. If you do not have such a file, one can be obtained by
+ // contacting Ron or Fermi Lab in Batavia IL, 60510, phone: 630-840-3000.
+ // $RCSfile: example_sub3.cc,v $
+ // rev="$Revision: 1.1 $$Date: 2015/04/24 17:05:47 $";
+
+#include "trace.h"
+#define TRACE_NAME "example_sub3"
+
+void example_sub4( void );
+
+void example_sub3( void )
+{   TRACE( 0, "hello from example_sub3 before calling sub4" );
+    example_sub4();
+    TRACE( 0, "hello from example_sub3 after  calling sub4" );
+}
--- trace/src_example/userspace/example_sub4.cc
+++ trace/src_example/userspace/example_sub4.cc
@@ -2,15 +2,13 @@
  // Mar 12, 2014. "TERMS AND CONDITIONS" governing this file are in the README
  // or COPYING file. If you do not have such a file, one can be obtained by
  // contacting Ron or Fermi Lab in Batavia IL, 60510, phone: 630-840-3000.
- // $RCSfile: example_sub2.cc,v $
- // rev="$Revision: 1.2 $$Date: 2015/04/24 17:05:47 $";
+ // $RCSfile: example_sub4.cc,v $
+ // rev="$Revision: 1.1 $$Date: 2015/04/24 17:05:47 $";
 
-#include "tracelib.h"
+#include "trace.h"
 
-void example_sub3( void );
-
-void example_sub2( void )
-{   TRACE( 0, "hello from example_sub2 before calling sub3" );
-    example_sub3();
-    TRACE( 0, "hello from example_sub2 after  calling sub3" );
+void example_sub4( void )
+{   TRACE( 0, "hello from example_sub4()" );
+#define TRACE_NAME "example_sub4"
+    TRACE( 0, "hello again from example_sub4()" );
 }

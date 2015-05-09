e466c91590b1e607887802097d07bd4c995d7074
ron <ron@f0fafde3-ccc7-4eb4-bb98-6daa41875d3e>
2015-05-09 12:19:17 +0000

    comment - that TRACE_NAME macro does not work in module environment, currently
    git-svn-id: svn+ssh://cdcvs.fnal.gov/cvs/projects/trace-svn/trunk@390 f0fafde3-ccc7-4eb4-bb98-6daa41875d3e

--- trace/src_example/module1/other_module.c
+++ trace/src_example/module1/other_module.c
@@ -3,7 +3,7 @@
     or COPYING file. If you do not have such a file, one can be obtained by
     contacting Ron or Fermi Lab in Batavia IL, 60510, phone: 630-840-3000.
     $RCSfile: other_module.c,v $
-    rev="$Revision: 1.1 $$Date: 2014-04-17 19:40:18 $";
+    rev="$Revision: 1.2 $$Date: 2015-05-09 12:19:17 $";
     */
 
 // NOTE: this is trace_.c and not trace.c because nfs server has case
@@ -15,7 +15,7 @@
 #include <linux/jiffies.h>
 #include <linux/delay.h>	/* msleep */
 
-
+#define TRACE_NAME "jones"  /* this currently does nothing :( */
 #include "trace.h"
 
  

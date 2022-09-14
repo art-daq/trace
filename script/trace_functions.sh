f93883dbbf55e61752e761633f4cadecebd9b30b
Ron Rechenmacher <ron@fnal.gov>
2022-09-14 16:57:07 -0500

    fix more robust tcolor

--- script/trace_functions.sh
+++ script/trace_functions.sh
@@ -4,8 +4,8 @@
 #   or COPYING file. If you do not have such a file, one can be obtained by
 #   contacting Ron or Fermi Lab in Batavia IL, 60510, phone: 630-840-3000.
 #   $RCSfile: trace.sh.functions,v $
-#   $Revision: 1560 $
-#   $Date: 2022-09-14 10:31:08 -0500 (Wed, 14 Sep 2022) $
+#   $Revision: 1563 $
+#   $Date: 2022-09-14 16:56:09 -0500 (Wed, 14 Sep 2022) $
 
 tcntl()   { trace_cntl "$@"; }
 tshow()   { test -n "${PAGER-}" && trace_cntl show "$@" | $PAGER || trace_cntl show "$@"; }
@@ -42,7 +42,21 @@ toffSg()   { trace_cntl lvlclrg 0 `bitN_to_mask "$@"` 0; }
 toffTg()   { trace_cntl lvlclrg 0 0 `bitN_to_mask "$@"`; }
 
 tenv()     { trace_envvars "$@"; }
-tcolor()   { trace_color "$@"; }
+tcolor()   { : $*=pids or other "token" which will have space before and after
+  : build sed filter;: defaults: 1=red 2=green 3=orange 4=blue 5=magenta 6=cyan 7=while 226=yellow ... 
+  test -n "${color_nums-}" || color_nums='1 2 3 4 5 6 7 226 93 87 189 195 177';: can put color_nums in env
+  num_color_nums=`echo $color_nums | wc -w`
+  test $# -eq 0 && { pid_list=`TRACE_SHOW="%x%P" trace_cntl show|awk '{print$1}'|head -300|sort -u | head -n$num_color_nums`; echo found `echo $pid_list | wc -w` unique PIDs in 1st 300 tshow lines >&2; set -- $pid_list; };: at most,could be less
+  test $# -eq 0 && { echo need at least 1 tid; return; }
+  tid=$1; esc=`printf "\033"`;: no shift to preserver num args to associate with arg with cnum
+  filt="s/\( $tid .*$\)/${esc}[38;5;`echo $color_nums|cut -d' ' -f1`m\1${esc}[0m/"
+  for num in `seq 2 $#`;do
+    tid=$2; shift
+    cnum=`echo $color_nums|cut -d' ' -f$num`
+    filt="$filt;s/\( $tid .*$\)/${esc}[38;5;${cnum}m\1${esc}[0m/"
+  done
+  sed -e "$filt"
+}
 
 tlvlsSave() {
   if [ $# -ne 1 ];then

444123eb966c339c7705c808486ca6cf54b4dc71
Ron Rechenmacher <ron@fnal.gov>
2024-01-17 15:15:04 -0600

    trace_cntl Revision

--- src_utility/trace_cntl.c
+++ src_utility/trace_cntl.c
@@ -4,7 +4,7 @@
     contacting Ron or Fermi Lab in Batavia IL, 60510, phone: 630-840-3000.
     $RCSfile: trace_cntl.c,v $
     */
-#define TRACE_CNTL_REV "$Revision: 1609 $$Date: 2024-01-16 16:01:45 -0600 (Tue, 16 Jan 2024) $"
+#define TRACE_CNTL_REV "$Revision: 1611 $$Date: 2024-01-17 15:14:16 -0600 (Wed, 17 Jan 2024) $"
 /*
 NOTE: This is a .c file instead of c++ mainly because C is friendlier when it
       comes to extended initializer lists.

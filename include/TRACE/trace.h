2f851b2c2bdd2548e4b43a5a510b2d901dac9b70
Ron Rechenmacher <ron@fnal.gov>
2024-12-09 14:42:01 -0600

    make TTEST lvl a DEBUG lvl

--- include/TRACE/trace.h
+++ include/TRACE/trace.h
@@ -8,7 +8,7 @@
 #define TRACE_H
 
 # if !defined(__CUDA_ARCH__) /* Allow inclusion into CUDA file (including .cu files) */
-#define TRACE_REV "$Revision: 1694 $$Date: 2024-12-05 11:50:58 -0600 (Thu, 05 Dec 2024) $"
+#define TRACE_REV "$Revision: 1695 $$Date: 2024-12-09 14:41:47 -0600 (Mon, 09 Dec 2024) $"
 
 // The C++ streamer style macros...............................................
 /*
@@ -198,7 +198,7 @@
 		struct { char tn[TRACE_TN_BUFSZ]; } _trc_;\
 	    int retval=0;\
 		if TRACE_INIT_CHECK(trace_name(TRACE_NAME,__TRACE_FILE__,_trc_.tn,sizeof(_trc_.tn))) {\
-			uint8_t lvl_ = (uint8_t)(lvl);\
+			uint8_t lvl_ = TLVL_DBG+(uint8_t)(lvl);\
 			if (  (traceControl_rwp->mode.bits.M && (traceLvls_p[traceTID].M & TLVLMSK(lvl_)))\
 	            ||(traceControl_rwp->mode.bits.S && (traceLvls_p[traceTID].S & TLVLMSK(lvl_)))) {\
 				retval=1;\
@@ -213,7 +213,7 @@
 	    int retval=0;\
 		if TRACE_INIT_CHECK(trace_name(TRACE_NAME,__TRACE_FILE__,_trc_.tn,sizeof(_trc_.tn))) {\
 			static TRACE_THREAD_LOCAL int tid_ = -1;				\
-			uint8_t lvl_ = (uint8_t)(lvl);\
+			uint8_t lvl_ = TLVL_DBG+(uint8_t)(lvl);\
 			if (tid_ == -1) tid_ = trace_tlog_name_(&(nam)[0],TRACE_NAME,__TRACE_FILE__,__FILE__,_trc_.tn,sizeof(_trc_.tn)); \
 			if (  (traceControl_rwp->mode.bits.M && (traceLvls_p[tid_].M & TLVLMSK(lvl_)))\
 	            ||(traceControl_rwp->mode.bits.S && (traceLvls_p[tid_].S & TLVLMSK(lvl_)))) {\
@@ -227,7 +227,7 @@
 # define TTEST(...)        [&](){\
     TSTREAMER_T_ _trc_((tlvle_t)(0), TRACE_GET_STATIC());\
     if (   TRACE_INIT_CHECK( trace_name(TRACE_NAME,__TRACE_FILE__,_trc_.tn,sizeof(_trc_.tn)) ) \
-        && (_trc_.TLOG3(__VA_ARGS__),((*_trc_.tidp != -1) || ((*_trc_.tidp= trace_tlog_name_(_trc_.nn,TRACE_NAME,__TRACE_FILE__,__FILE__,_trc_.tn,sizeof(_trc_.tn))) != -1))) \
+        && (_trc_.TLOG3_DEBUG(__VA_ARGS__),((*_trc_.tidp != -1) || ((*_trc_.tidp= trace_tlog_name_(_trc_.nn,TRACE_NAME,__TRACE_FILE__,__FILE__,_trc_.tn,sizeof(_trc_.tn))) != -1))) \
         && trace_do_streamer(&_trc_)) {\
             return (1);\
         } else return (0);\
@@ -274,7 +274,7 @@ enum tlvle_t { TRACE_LVL_ENUM_0_9, TRACE_LVL_ENUM_10_63 };
 #endif
 
 // clang-format off
-#define TRACE_REVx $_$Revision: 1694 $_$Date: 2024-12-05 11:50:58 -0600 (Thu, 05 Dec 2024) $
+#define TRACE_REVx $_$Revision: 1695 $_$Date: 2024-12-09 14:41:47 -0600 (Mon, 09 Dec 2024) $
 // Who would ever have an identifier/token that begins with $_$???
 #define $_$Revision  0?0
 #define $_$Date      ,
